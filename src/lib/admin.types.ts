08aa5e3104c25a4b86b71ff6600a46ffda2df828
User <user@example.com>
2025-10-14 22:55:06 +0800

    "Update"

---
+++ src/lib/admin.types.ts
@@ -0,0 +1,29 @@
+export interface AdminConfig {
+  SiteConfig: {
+    SiteName: string;
+    Announcement: string;
+    SearchDownstreamMaxPage: number;
+    SiteInterfaceCacheTime: number;
+  };
+  UserConfig: {
+    AllowRegister: boolean;
+    Users: {
+      username: string;
+      role: 'user' | 'admin' | 'owner';
+      banned?: boolean;
+    }[];
+  };
+  SourceConfig: {
+    key: string;
+    name: string;
+    api: string;
+    detail?: string;
+    from: 'config' | 'custom';
+    disabled?: boolean;
+  }[];
+}
+
+export interface AdminConfigResult {
+  Role: 'owner' | 'admin';
+  Config: AdminConfig;
+}
